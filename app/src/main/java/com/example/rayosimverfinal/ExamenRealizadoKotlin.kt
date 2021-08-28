b25cad0aa18b04eb9807d55df85c8965c7d2123d
IvanPino <pinoantivan@gmail.com>
2021-08-28 15:09:05 -0400

    Todos los toast listos

---
+++ app/src/main/java/com/example/rayosimverfinal/ExamenRealizadoKotlin.kt
@@ -0,0 +1,13 @@
+package com.example.rayosimverfinal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ExamenRealizadoKotlin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_examen_realizado_kotlin)
+    }
+}
+
+//HACER BOTON QUE MANDE A PANTALLA DE INICIO
